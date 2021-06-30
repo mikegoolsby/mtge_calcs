7d608c79f4cdb18d3832eac909cc4bc41a029ad3
mikegoolsby <55331311+mikegoolsby@users.noreply.github.com>
2021-06-30 16:35:48 -0400

    finalizes credit support calculation

--- calculateCreditSupport.py
+++ calculateCreditSupport.py
@@ -1,22 +1,25 @@
 def calculateCreditSupport():
-    '''
-    calculated as (Reserve + tranche + (collat balance - group balance)) / Collat Balance
-    
-    '''
-    reserve_bal = int(input("enter reserve balance: "))
+
     raw_input = (input("enter suboordinate tranche balances separated by a space: "))
+    reserve_bal = int(input("enter reserve balance: "))
+    
     tranche_bal = raw_input.split()
 
     for i in range(len(tranche_bal)):
         tranche_bal[i] = int(tranche_bal[i])
 
 
-    tranche_sums = sum(tranche_bal)    
-    group_bal = int(input("enter group balance: "))
-    collat_bal = int(input("enter overall collateral balance: "))
-    oc = collat_bal - group_bal
-    print("\n")
-    return (reserve_bal + tranche_sums + oc) / collat_bal
+    tranche_sums = sum(tranche_bal)
 
-print(calculateCreditSupport())
-        
+    group_bal = int(input("enter group balance: "))
+
+    collat_bal = int(input("enter overall collateral balance: "))
+
+    oc = (collat_bal - group_bal) / collat_bal
+
+    oc_dollar = oc * collat_bal
+
+    print("\n")
+    return print((reserve_bal + tranche_sums + oc_dollar) / collat_bal)
+
+calculateCreditSupport()
